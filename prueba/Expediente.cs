2895cbbd55524dc5c7112d2811d188738b423f5d
JagerSorel <vargasfabricio1989@gmail.com>
2026-05-22 15:19:28 -0600

    Prototipo obtención de ganancias

--- prueba/Expediente.cs
+++ prueba/Expediente.cs
@@ -16,35 +16,60 @@ namespace prueba
         public Expediente()
         {
             InitializeComponent();
+            lblFecha.Text = Convert.ToString(DateTime.Now);
+            ObtDatos();
             GrafGastos();
             GrafGanancias();
             GrafIngresos();
 
         }
+        private void ObtDatos()
+        {
+            double []ingresos = { 44.65, 43.33, 21.32, 64.8 };
+            double []gastos = { 46.45, 41.33, 12.32, 48.6 };
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Ganancias/Pérdidas");
+            for (int i = 0; i < ingresos.Length; i++)
+            {
+                dt.Rows.Add("Enero", Convert.ToString(ingresos[i]), Convert.ToString(gastos[i]), Convert.ToString(ingresos[i] - gastos[i]));
+            }
+        }
         private void GrafGastos()
         {
             var series = new Series("Gastos");
             series.ChartType = SeriesChartType.Line;
-            series.Points.DataBindXY(new[] { 1, 2, 3, 4 }, new[] { 100, 200, 90, 150 });
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { dgvExpediente});
             chaGastos.Series.Add(series);
         }
         private void GrafGanancias()
         {
             var series = new Series("Ganancias");
             series.ChartType = SeriesChartType.Line;
-            series.Points.DataBindXY(new[] { 1, 2, 3, 4 }, new[] { 100, 200, 90, 150 });
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 100, 200, 90, 150 });
             chaGanancias.Series.Add(series);
         }
         private void GrafIngresos()
         {
             var series = new Series("Ingresos");
             series.ChartType = SeriesChartType.Line;
-            series.Points.DataBindXY(new[] { 1, 2, 3, 4 }, new[] { 100, 200, 90, 150 });
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 100, 200, 90, 150 });
             chaIngresos.Series.Add(series);
         }
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Expediente_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'safeWealthFinanceDBDataSet.ExpedienteSemestral' Puede moverla o quitarla según sea necesario.
+            this.expedienteSemestralTableAdapter.Fill(this.safeWealthFinanceDBDataSet.ExpedienteSemestral);
+
         }
     }
 }
