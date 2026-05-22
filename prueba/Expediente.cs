677c4b3ea5e18efed7584c2dbb24be49fbc937f3
JagerSorel <vargasfabricio1989@gmail.com>
2026-05-22 17:18:13 -0600

    Estamos cerca

--- prueba/Expediente.cs
+++ prueba/Expediente.cs
@@ -17,42 +17,47 @@ namespace prueba
         {
             InitializeComponent();
             lblFecha.Text = Convert.ToString(DateTime.Now);
-            ObtDatos();
+            OrgDatosTabla();
             GrafGastos();
             GrafGanancias();
             GrafIngresos();
 
         }
-        private void ObtDatos()
+        public void OrgDatosTabla()
         {
-            double []ingresos = { 44.65, 43.33, 21.32, 64.8 };
-            double []gastos = { 46.45, 41.33, 12.32, 48.6 };
+            double []ingresos = { 46.45, 41.33, 12.32, 48.6, 43.32, 61.37 };
+            double []gastos = { 0, 0, 0, 0, 0, 0 };
             DataTable dt = new DataTable();
             dt.Columns.Add("Ganancias/Pérdidas");
-            for (int i = 0; i < ingresos.Length; i++)
+            for (int i = 0; i <= ingresos.Length; i++)
             {
                 dt.Rows.Add("Enero", Convert.ToString(ingresos[i]), Convert.ToString(gastos[i]), Convert.ToString(ingresos[i] - gastos[i]));
             }
         }
         private void GrafGastos()
         {
+            double[] gastos = { 0, 0, 0, 0, 0, 0 };
             var series = new Series("Gastos");
             series.ChartType = SeriesChartType.Line;
-            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { dgvExpediente});
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { gastos[0], gastos[1], gastos[2], gastos[3], gastos[4], gastos[5] });
+            chaGanancias.Series.Add(series);
             chaGastos.Series.Add(series);
         }
         private void GrafGanancias()
         {
+            double[] ingresos = { 46.45, 41.33, 12.32, 48.6, 43.32, 61.37 };
             var series = new Series("Ganancias");
             series.ChartType = SeriesChartType.Line;
-            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 100, 200, 90, 150 });
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { ingresos[0], ingresos[1], ingresos[2], ingresos[3], ingresos[4], ingresos[5] });
             chaGanancias.Series.Add(series);
         }
         private void GrafIngresos()
         {
+            double[] ingresos = { 46.45, 41.33, 12.32, 48.6, 43.32, 61.37 };
+            double[] gastos = { 0, 0, 0, 0, 0, 0 };
             var series = new Series("Ingresos");
             series.ChartType = SeriesChartType.Line;
-            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 100, 200, 90, 150 });
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6 }, new[] { (ingresos[0] - gastos[0]), (ingresos[1] - gastos[1]), (ingresos[2] - gastos[2]), (ingresos[3] - gastos[3]), (ingresos[4] - gastos[4]), (ingresos[5] - gastos[5]) });
             chaIngresos.Series.Add(series);
         }
         private void btnSalir_Click(object sender, EventArgs e)
